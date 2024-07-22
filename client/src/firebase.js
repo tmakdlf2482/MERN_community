39b611352d8da27678c4282c2bc50fd3a92eb530
DongYoon <tmakdlf2482@naver.com>
2024-07-22 23:46:10 +0900

    hide firebase api keys

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,17 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import dev from '../config/dev.js'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDd_QE7txmn30JPJ8jXwwUvwk3I-9Yq6cA",
-  authDomain: "react-community-829d0.firebaseapp.com",
-  projectId: "react-community-829d0",
-  storageBucket: "react-community-829d0.appspot.com",
-  messagingSenderId: "859016357479",
-  appId: "1:859016357479:web:40b9a3e3406d6c9c4e56c2",
+  apiKey: dev.apiKey,
+  authDomain: dev.authDomain,
+  projectId: dev.projectId,
+  storageBucket: dev.storageBucket,
+  messagingSenderId: dev.messagingSenderId,
+  appId: dev.appId,
 };
 
 // Initialize Firebase
