4cd25cae8171140963108b53d7f5751fc8233b7a
dobby <tmakdlf2482@naver.com>
2024-08-11 15:20:00 +0900

    nickname duplication check

--- client/src/Component/User/Register.jsx
+++ client/src/Component/User/Register.jsx
@@ -11,6 +11,8 @@ function Register() {
   const [PW, setPW] = useState("");
   const [PWConfirm, setPWConfirm] = useState("");
   const [Flag, setFlag] = useState(false);
+  const [NameCheck, setNameCheck] = useState(false); // 닉네임 중복검사 state
+  const [NameInfo, setNameInfo] = useState("");
 
   let navigate = useNavigate();
 
@@ -23,6 +25,9 @@ function Register() {
     }
     if (PW != PWConfirm) {
       return alert('비밀번호와 비밀번호 확인 값은 같아야 합니다!');
+    }
+    if (!NameCheck) {
+      return alert('닉네임 중복검사를 진행해 주세요!');
     }
     
     let createdUser = await firebase.auth().createUserWithEmailAndPassword(Email, PW); // firebase가 회원가입 될떄 까지 잠시 대기
@@ -54,11 +59,38 @@ function Register() {
     });
   };
 
+  const NameCheckFunc = (e) => {
+    e.preventDefault();
+
+    if (!Name) {
+      return alert('닉네임을 입력해주세요!');
+    }
+
+    let body = {
+      displayName: Name,
+    };
+
+    axios.post('/api/user/namecheck', body)
+    .then((response) => {
+      if (response.data.success) {
+        if (response.data.check) {
+          setNameCheck(true);
+          setNameInfo('사용가능한 닉네임입니다.');
+        }
+        else {
+          setNameInfo('중복된 닉네임입니다.');
+        }
+      }
+    });
+  };
+
   return (
     <LoginDiv>
       <form>
-        <label>이름</label>
-        <input type="name" value={Name} autoFocus onChange={(e) => {setName(e.target.value)}}/>
+        <label>닉네임</label>
+        <input type="name" value={Name} autoFocus onChange={(e) => {setName(e.target.value)}} disabled={NameCheck}/>
+        {NameInfo}
+        <button onClick={(e) => {NameCheckFunc(e)}}>닉네임 중복검사</button>
 
         <label>이메일</label>
         <input type="email" value={Email} onChange={(e) => {setEmail(e.target.value)}} />
